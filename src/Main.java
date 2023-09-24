6185b83a326e7cb78fc9a4a488f1c8341e3f1e43
Rehab <rehabmostafa.j@gmail.com>
2023-09-24 18:32:27 +0300

    changes

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Navy blue color");
+        System.out.println("Cashmere color");
+        System.out.println("Indigo color");
+    }
+}

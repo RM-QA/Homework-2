e111740ac3eb4478855b0b8fa3cf154d1b95937f
Rehab <rehabmostafa.j@gmail.com>
2023-09-30 00:21:50 +0300

    lesson 3 practice

--- src/MyClass.java
+++ src/MyClass.java
@@ -1,10 +1,24 @@
-public class MyClass{
+public class MyClass {
     public static void main(String[] args) {
 
         System.out.println("  *");
         System.out.println("* * *");
         System.out.println(" * *");
-        System.out.println("*");
+
+
+        // lesson 3 practice
+
+        double firstNumber = 1;
+        double secondNumber = 2;
+        double sum = firstNumber + secondNumber;
+        System.out.println(firstNumber);
+        System.out.println(secondNumber);
+        System.out.println(sum);
+
+        // Declaration boolean meaning
+        boolean isBigger;
+        isBigger = firstNumber > secondNumber;
+        System.out.println(isBigger);
 
     }
 }
